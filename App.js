c1a92ffa1cf3fa27df7699d5cb9715d3f6d54efd
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2020-05-11 22:22:19 +0200

    Add a stack navigator and a game screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
@@ -9,65 +10,75 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  fill: {
+    alignSelf: 'stretch',
+  },
+  startScreenTop: {
+    backgroundColor: '#3c3b6e',
+  },
+  startScreenTopText: {
+    color: '#ffffff',
+    fontSize: 80,
+    fontWeight: '900',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+  startScreenBottom: {
+    backgroundColor: '#b22234',
+  },
+  startScreenBottomText: {
+    textTransform: 'uppercase',
+    fontWeight: '900',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderColor: '#ffffff',
+    borderWidth: 4,
+    color: '#ffffff',
+    padding: 20,
+  },
 });
 
-function StartScreenTopSection() {
+function GameScreen() {
   return (
-    <View
-      style={[
-        styles.center,
-        {alignSelf: 'stretch', backgroundColor: '#3c3b6e'},
-      ]}>
-      <Text
-        style={{
-          color: '#ffffff',
-          fontSize: 80,
-          fontWeight: '900',
-          textAlign: 'center',
-          textTransform: 'uppercase',
-        }}>
-        Biden's Run
-      </Text>
+    <View style={styles.center}>
+      <Text>Game screen</Text>
     </View>
   );
 }
 
-function StartScreenBottomSection() {
+function StartScreen({navigation}) {
   return (
-    <View
-      style={[
-        styles.center,
-        {alignSelf: 'stretch', backgroundColor: '#b22234'},
-      ]}>
-      <TouchableHighlight
-        activeOpacity={0.6}
-        underlayColor="#b22234"
-        onPress={() => alert('Pressed!')}>
-        <Text
-          style={{
-            textTransform: 'uppercase',
-            fontWeight: '900',
-            fontSize: 20,
-            borderStyle: 'solid',
-            borderColor: '#ffffff',
-            borderWidth: 4,
-            color: '#ffffff',
-            padding: 20,
-          }}>
-          Press to play
-        </Text>
-      </TouchableHighlight>
+    <View style={styles.center}>
+      <View style={[styles.center, styles.fill, styles.startScreenTop]}>
+        <Text style={[styles.startScreenTopText]}>Biden's Run</Text>
+      </View>
+      <View style={[styles.center, styles.fill, styles.startScreenBottom]}>
+        <TouchableHighlight
+          activeOpacity={0.6}
+          underlayColor="#b22234"
+          onPress={() => navigation.navigate('Game')}>
+          <Text style={[styles.startScreenBottomText]}>Press to play</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.center}>
-        <StartScreenTopSection />
-        <StartScreenBottomSection />
-      </View>
+      <Stack.Navigator
+        initialRouteName="Start"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Start" component={StartScreen} />
+        <Stack.Screen
+          name="Game"
+          component={GameScreen}
+          options={{gestureEnabled: false}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
