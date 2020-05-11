e601a0e6195f2ca1f702a220636524360993e741
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2020-05-11 16:28:33 +0200

    Add start button

--- App.js
+++ App.js
@@ -1,114 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
-export default App;
+function StartScreenTopSection() {
+  return (
+    <View
+      style={[
+        styles.center,
+        {alignSelf: 'stretch', backgroundColor: '#3c3b6e'},
+      ]}>
+      <Text
+        style={{
+          color: '#ffffff',
+          fontSize: 80,
+          fontWeight: '900',
+          textAlign: 'center',
+          textTransform: 'uppercase',
+        }}>
+        Biden's Run
+      </Text>
+    </View>
+  );
+}
+
+function StartScreenBottomSection() {
+  return (
+    <View
+      style={[
+        styles.center,
+        {alignSelf: 'stretch', backgroundColor: '#b22234'},
+      ]}>
+      <TouchableHighlight
+        activeOpacity={0.6}
+        underlayColor="#b22234"
+        onPress={() => alert('Pressed!')}>
+        <Text
+          style={{
+            textTransform: 'uppercase',
+            fontWeight: '900',
+            fontSize: 20,
+            borderStyle: 'solid',
+            borderColor: '#ffffff',
+            borderWidth: 4,
+            color: '#ffffff',
+            padding: 20,
+          }}>
+          Press to play
+        </Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+
+function StartScreen() {
+  return (
+    <View style={styles.center}>
+      <StartScreenTopSection />
+      <StartScreenBottomSection />
+    </View>
+  );
+}
+
+export default StartScreen;
